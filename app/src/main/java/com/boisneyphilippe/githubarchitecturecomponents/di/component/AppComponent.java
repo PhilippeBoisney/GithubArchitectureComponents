58b2b0bf4f6391391a4790306aceb1da423ca625
Ashif Mujtoba <nmujtoba@gmail.com>
2018-03-20 17:07:45 +0600

    Seperated network and Room module. Added Timber log in network call

--- app/src/main/java/com/boisneyphilippe/githubarchitecturecomponents/di/component/AppComponent.java
+++ app/src/main/java/com/boisneyphilippe/githubarchitecturecomponents/di/component/AppComponent.java
@@ -4,8 +4,10 @@ import android.app.Application;
 
 import com.boisneyphilippe.githubarchitecturecomponents.App;
 import com.boisneyphilippe.githubarchitecturecomponents.di.module.ActivityModule;
-import com.boisneyphilippe.githubarchitecturecomponents.di.module.AppModule;
 import com.boisneyphilippe.githubarchitecturecomponents.di.module.FragmentModule;
+import com.boisneyphilippe.githubarchitecturecomponents.di.module.NetworkModule;
+import com.boisneyphilippe.githubarchitecturecomponents.di.module.RoomModule;
+import com.boisneyphilippe.githubarchitecturecomponents.di.module.ViewModelModule;
 
 import javax.inject.Singleton;
 
@@ -17,7 +19,13 @@ import dagger.Component;
  */
 
 @Singleton
-@Component(modules={ActivityModule.class, FragmentModule.class, AppModule.class})
+@Component(modules = {
+        ActivityModule.class,
+        FragmentModule.class,
+        ViewModelModule.class,
+        NetworkModule.class,
+        RoomModule.class
+})
 public interface AppComponent {
 
     @Component.Builder
